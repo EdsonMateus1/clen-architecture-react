9cceb30f72f6195e91bcfaee8eddb0f20d5b3b61
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2022-03-29 18:47:36 -0300

    test: remote authentication

---
+++ src/data/userCases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,19 @@
+import { RemoteAuthentication } from "./remote-authentication";
+import { HttpPostClient } from "data/protocols/http/http-post-client";
+
+describe("remoteAuthentication", () => {
+  test("Should call httpClient with correct url", async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+      }
+    }
+    const url = "any_url";
+    const httpPostClientSpy = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    await sut.auth();
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});
